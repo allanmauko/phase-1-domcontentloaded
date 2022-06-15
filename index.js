b91a5ad3bdba8d89068f6870d644603274c444fd
allanmauko <allan.mauko@student.moringaschool.com>
2022-06-15 10:54:54 +0300

    commit tree b013c100d8a3fbee71ac0060a9f8c8dd26ad117a

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+    document.addEventListener('DOMContentLoaded', function(){
+     loadedDOM();
+    })
+
+    function loadedDOM(){
+        document.getElementById('text').textContent = 'This is really cool!';
+    }
